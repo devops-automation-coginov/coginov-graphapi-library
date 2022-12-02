6ed0e798ce44fa4e41ee17ccb7ee788da0c2cf77
alekclopez <alekclopez@gmail.com>
2022-12-02 09:25:55 -0500

    Update sign script with the folder of DLL

--- Coginov.GraphApi.Library.Azure.Pipelines/signcoginovgraphapilibrary.ps1
+++ Coginov.GraphApi.Library.Azure.Pipelines/signcoginovgraphapilibrary.ps1
@@ -5,8 +5,8 @@ param (
     [string]$base64
 )
 
-cd -Path "$ArtefactFolder\Coginov.Semantic.Library\bin\Debug\netstandard2.1\" -PassThru
-$fileExec = "$ArtefactFolder\Coginov.Semantic.Library\bin\Debug\netstandard2.1\Coginov.Semantic.Library.dll";
+cd -Path "$ArtefactFolder\bin\Debug\net6.0\" -PassThru
+$fileExec = "$ArtefactFolder\bin\Debug\net6.0\Coginov.GraphApi.Library.dll";
 $buffer = [System.Convert]::FromBase64String($base64);
 $certificate = [System.Security.Cryptography.X509Certificates.X509Certificate2]::new($buffer);
 Set-AuthenticodeSignature -FilePath $fileExec -Certificate $certificate;
