656f0f2d65ff32d15d5fe21913c9702fece27ade
devops-automation-coginov <devops_automation@coginov.com>
2025-01-15 09:38:23 -0500

    Update signcoginovgraphapilibrary.ps1

--- Coginov.GraphApi.Library.Azure.Pipelines/signcoginovgraphapilibrary.ps1
+++ Coginov.GraphApi.Library.Azure.Pipelines/signcoginovgraphapilibrary.ps1
@@ -10,3 +10,18 @@ $fileExec = "$ArtefactFolder\bin\Debug\net8.0\Coginov.GraphApi.Library.dll";
 $buffer = [System.Convert]::FromBase64String($base64);
 $certificate = [System.Security.Cryptography.X509Certificates.X509Certificate2]::new($buffer);
 Set-AuthenticodeSignature -FilePath $fileExec -Certificate $certificate;
+
+$certPath = "$ArtefactFolder\tempCert.pfx"
+[System.IO.File]::WriteAllBytes($certPath, $buffer)
+
+# Firmar el ensamblado usando signtool con SHA-256
+Start-Process -FilePath "signtool.exe" -ArgumentList @(
+    "sign",
+    "/f", $certPath,
+    "/fd", "SHA256",
+    "/tr", "http://timestamp.digicert.com",
+    "/td", "SHA256",
+    $fileExec
+) -Wait
+
+Remove-Item $certPath -Force
